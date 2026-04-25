29bcf2bdb697bdf341a51bdc94bdafa92924682b
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2026-04-25 10:49:31 -0500

    feat: add basic registration form UI

---
+++ InTheCupboard/src/routes/register/+page.svelte
@@ -0,0 +1,25 @@
+<form method="POST" class="border rounded-xl grid grid-cols-2 grid-rows-3 gap-5 w-2/5 px-10 pt-10 mx-auto mt-20">
+    <div class="">
+        <label for="firstName" class="block">First Name</label>
+        <input name="firstName" type="text" class="border rounded-lg pl-2">
+    </div>
+
+    <div class="">
+        <label for="lastName" class="block">Last Name</label>
+        <input name="lastName" type="text" class="border rounded-lg pl-2">
+    </div>
+
+    <div class="">
+        <label for="email" class="block">Email</label>
+        <input name="" type="text" class="border rounded-lg pl-2">
+    </div>
+
+    <div class="">
+        <label for="password" class="block">Password</label>
+        <input name="password" type="password" class="border rounded-lg pl-2">
+    </div>
+
+    <div class="col-span-full flex justify-center content-center">
+        <button class="border rounded-md px-2 my-2">Create Account</button>
+    </div>
+</form>
