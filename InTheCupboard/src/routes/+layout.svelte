be19d2d4d4b461aee4bfe32ce4a6cfdb081c5839
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2026-04-25 10:26:18 -0500

    chore: add favicon

--- InTheCupboard/src/routes/+layout.svelte
+++ InTheCupboard/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/in-the-cupboard-text.png';
 
 	let { children } = $props();
 </script>
