591f458ebb72899c6f923c4e7c92ea23d07dcdef
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2026-04-25 14:41:42 -0500

    feat: add basic validation and redirect to login page

---
+++ InTheCupboard/src/routes/login/+page.server.js
@@ -0,0 +1,38 @@
+import { fail, redirect } from "@sveltejs/kit";
+
+export const actions = {
+    default: async ({ request }) => {
+        const data = await request.formData();
+
+        const email = data.get('email');
+        const password = data.get('password');
+
+        if (!email) {
+            // add the right status code later
+            return fail(400, { emailMissing: true });
+        }
+
+        if (!password) {
+            // add the right status code later
+            return fail(400, { passwordMissing: true, email })
+        }
+
+        // make sure we got the data we want
+
+
+
+
+        // look up user in DB based on email
+        // if no user, return fail to user
+
+        // if user found
+        //     hash password
+        //     compare password hashes
+    
+        // if password is incorrect, return fail to user
+        // else pass JWT cookie back to user
+
+        // once authenticated, redirect to home page
+        redirect(303, "/");
+    }
+}
