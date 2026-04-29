5837e512ba45accb278b3354e0899a3819dcb3b5
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2026-04-28 23:44:28 -0500

    feat: implement user insertion in registration

--- InTheCupboard/src/routes/(pre-auth)/register/+page.server.js
+++ InTheCupboard/src/routes/(pre-auth)/register/+page.server.js
@@ -1,40 +1,63 @@
 import { fail, redirect } from '@sveltejs/kit';
+import { hashPassword } from '$lib/auth.js';
+import { connectToDatabase, createUser } from '$lib/db.js';
+import { hash } from 'bcrypt';
 
 export const actions = {
     default: async ({request}) => {
+        
         const data = await request.formData();
-
-        const user = {
-            name: {
-                first: data.get("firstName"),
-                last: data.get("lastName")
-            },
-            email: data.get("email"),
-            password: data.get("password")
-        };
+        const firstName = data.get("firstName");
+        const lastName = data.get("lastName");
+        const email = data.get("email");
+        const password = data.get("password");
 
         // check that we got the data we wanted
-        if (!user.name.first) {
+        if (!firstName) {
             console.log("returning due to bad name input")
             // make this the right status code later
-            return fail(406, { nameMissing: true, name: user.name, email: user.email})
+            return fail(406, { nameMissing: true, firstName, lastName, email});
         }
-        if (!user.email) {
+        if (!email) {
             // make this the right status code later
-            return fail(400, {emailMissing: true, name: user.name})
+            return fail(400, {emailMissing: true, firstName, lastName})
         }
-        if (!user.password) {
+        if (!password) {
             // make this the right status code later
-            return fail(400, {passwordMissing: true, name: user.name, email: user.email })
+            return fail(400, {passwordMissing: true, firstName, lastName, email})
         }
-        // throw an error if data is missing
-            // make the error clear
-            // maybe make a function somewhere else and import so we can also write tests against it
 
-        // add user account to the db
-            // hash password
+        // TODO: make sure email is an email
 
-        // redirect to the login page
+        // TODO: make sure password meets security requirements (length, not simple)
+        
+        try {            
+            const password_hash = await hashPassword(password);
+            const user = {
+                name: {
+                    first: firstName,
+                    last: lastName,
+                },
+                email,
+                password_hash
+            }
+    
+            const db = connectToDatabase("local.db");
+            createUser(db, user);
+        } catch (error) {
+            if (error.code && error.code === 'SQLITE_CONSTRAINT_UNIQUE') {
+                console.error("Unique Constraint Violation");
+                return fail(400, {userAlreadyExists: true, firstName, lastName, email});
+            }
+            if (error.code) {
+                console.error("SQL ERROR: ", error.code);
+                return fail(400, {otherError: true, firstName, lastName, email});
+            }
+
+            console.error("ERROR: ", error);
+            return fail(400, {otherError: true, firstName, lastName, email})
+        }
+
         redirect(303, "/login");
     }
 }
--- InTheCupboard/src/routes/(pre-auth)/register/+page.svelte
+++ InTheCupboard/src/routes/(pre-auth)/register/+page.svelte
@@ -5,19 +5,21 @@
 <form method="POST" class="border rounded-xl grid grid-cols-2 grid-rows-3 gap-5 w-2/5 px-10 pt-10 pb-4 mx-auto mt-20">
     <div class="">
         <label for="firstName" class="block capitalize">First Name</label>
-        <input name="firstName" type="text" class="border rounded-lg pl-2" value={form?.name?.first ?? ""}>
+        <input name="firstName" type="text" class="border rounded-lg pl-2" value={form?.firstName ?? ""}>
     </div>
 
     <div class="">
         <label for="lastName" class="block">Last Name</label>
-        <input name="lastName" type="text" class="border rounded-lg pl-2" value={form?.name?.last ?? ""}>
+        <input name="lastName" type="text" class="border rounded-lg pl-2" value={form?.lastName ?? ""}>
     </div>
 
+    <!-- TODO: implement email requirement -->
     <div class="">
         <label for="email" class="block">Email</label>
         <input name="email" type="text" class="border rounded-lg pl-2" value={form?.email ?? ""}>
     </div>
 
+    <!-- TODO: implement password validations (length, etc) -->
     <div class="">
         <label for="password" class="block">Password</label>
         <input name="password" type="password" class="border rounded-lg pl-2">
@@ -29,4 +31,6 @@
     {#if form?.nameMissing}<p class="text-red-600 col-span-full mx-auto">Name is a required field</p>{/if}
     {#if form?.emailMissing}<p class="text-red-600 col-span-full mx-auto">Email is a required field</p>{/if}
     {#if form?.passwordMissing}<p class="text-red-600 col-span-full mx-auto">Password is a required field</p>{/if}
+    {#if form?.userAlreadyExists}<p class="text-red-600 col-span-full mx-auto" >A user with this email already exists. Please login</p>{/if}
+    {#if form?.otherError}<p class="text-red-600 col-span-full mx-auto">An Unexpected Error Occurred</p>{/if}
 </form>
