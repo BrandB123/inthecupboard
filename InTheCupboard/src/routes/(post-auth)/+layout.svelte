4c466fb60382f4cc70d81d4dfc3c5f9b9d3a5463
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2026-05-08 21:00:30 -0500

    feat: allow users to logout

--- InTheCupboard/src/routes/(post-auth)/+layout.svelte
+++ InTheCupboard/src/routes/(post-auth)/+layout.svelte
@@ -5,9 +5,10 @@
 <nav class="flex mx-6 my-6">
 	<h1 class="font-serif text-3xl">In The Cupboard</h1>
 	<div class="ml-auto mr-6 flex gap-6 items-end">
-		<a href="/register">Add Dish</a>
-		<a href="/login">Grocery List</a>
-		<a href="/register">Search</a>
+		<a href="/add-dish">Add Dish</a>
+		<a href="/grocery-list">Grocery List</a>
+		<a href="/search">Search</a>
+        <a href="/logout" data-sveltekit-preload-data="tap">Log out</a>
 	</div>
 </nav>
 {@render children()}
