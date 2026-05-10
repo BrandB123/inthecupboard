0b55587d06af1218593954e73711ab19f05bf647
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2026-05-09 23:48:19 -0500

    feat: add basic home page styling

--- InTheCupboard/src/routes/(post-auth)/home/+page.svelte
+++ InTheCupboard/src/routes/(post-auth)/home/+page.svelte
@@ -7,13 +7,20 @@
         <p>{data.message}</p>
     {/if}
     
-    {#if data.dishes?.length > 0}       
-        {#each data.dishes as dish}
-            <p>{dish.name}</p>
-        {/each}
-    {:else}
-        <p>Please add a dish to view it here</p>
-    {/if}
+    <div class="w-1/3 border rounded-xl px-8 pt-4 pb-8 mx-auto mt-20 min-h-[50vh]">
+        <h2 class="text-3xl text-center border-b mb-4 pb-2 ">Dishes</h2>
+        <ul class="list-disc">
+            {#if data.dishes?.length > 0}
+                {#each data.dishes as dish}
+                    <li class="ml-8">
+                        <a href="/dishes/{dish?.name.toLowerCase()}">{dish?.name}</a>
+                    </li>
+                {/each}
+            {:else}
+                <p>Please add a dish to view it here</p>
+            {/if}
+        </ul>
+    </div>
 
     
 </div>
