731fcbba588a09e6533baa2ff338454de6a8b9aa
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2026-05-19 16:36:33 -0500

    feat: filter dishes by category

--- InTheCupboard/src/routes/(post-auth)/home/+page.svelte
+++ InTheCupboard/src/routes/(post-auth)/home/+page.svelte
@@ -1,5 +1,9 @@
 <script>
     let { data } = $props();
+    let category = $state();
+    
+    const allDishes = data?.dishes;
+    
 </script>
 
 <div>
@@ -9,16 +13,24 @@
     
     <div class="w-1/3 border rounded-xl px-8 pt-4 pb-8 mx-auto mt-20 min-h-[50vh]">
         <h2 class="text-3xl text-center border-b mb-4 pb-2 ">Dishes</h2>
+        <select bind:value={category} class="border rounded-lg block ml-auto text-center">
+            <option value="all" selected>All</option>
+            <option value="main">Main</option>
+            <option value="side">Side</option>
+            <option value="dessert">Dessert</option>
+        </select>
         <ul class="list-disc">
-            {#if data.dishes?.length > 0}
-                {#each data.dishes as dish}
-                    <li class="ml-8">
-                        <a 
-                            href="/dishes/{dish?.name.toLowerCase()}"
-                            class="capitalize">
-                            {dish?.name}
-                        </a>
-                    </li>
+            {#if allDishes.length > 0}
+                {#each allDishes as dish}
+                   {#if dish.category === category || category === "all"}
+                       <li class="ml-8">
+                               <a 
+                                   href="/dishes/{dish?.name.toLowerCase()}"
+                                   class="capitalize">
+                                   {dish?.name}
+                               </a>
+                        </li>
+                   {/if}
                 {/each}
             {:else}
                 <p>Please add a dish to view it here</p>
