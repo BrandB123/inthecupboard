df21c1e5de239f3f97c4559a3635fa291240b6a9
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2026-06-06 23:37:53 -0500

    feat: style login page layout

--- InTheCupboard/src/routes/(pre-auth)/login/+layout.svelte
+++ InTheCupboard/src/routes/(pre-auth)/login/+layout.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <nav class="flex mx-6 my-6">
-	<h1 class="font-serif text-3xl">In The Cupboard</h1>
-	<div class="ml-auto mr-6 flex gap-6 items-end">
+	<h1 class="font-serif flex-1 text-3xl">In The Cupboard</h1>
+	<div class="ml-auto flex-1 flex gap-6 justify-end items-end pt-3 pr-3 text-md lg:text-lg">
 		<a href="/register">Create Account</a>
 	</div>
 </nav>
